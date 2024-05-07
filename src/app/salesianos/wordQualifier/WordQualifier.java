332640c737da735494cfa0c9104504316ac0ffa7
the-albertox <126242013+The-Albertox@users.noreply.github.com>
2024-05-07 12:12:30 +0100

    feat(wordQualifier): create gettter and setter and the constructor

--- src/app/salesianos/wordQualifier/WordQualifier.java
+++ src/app/salesianos/wordQualifier/WordQualifier.java
@@ -1,5 +1,23 @@
 package app.salesianos.wordQualifier;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class WordQualifier {
+    private Map<Character, List<String>> wordByInitial;
+
+    public WordQualifier(){
+        wordByInitial   = new HashMap<>();
+    }
+
+    public Map<Character, List<String>> getWordByInitial(){
+        return wordByInitial;
+    }
+
+    public void setWordByInitial(Map<Character, List<String>> wordByInitial){
+        this.wordByInitial = wordByInitial;
+    }
+
     
 }
