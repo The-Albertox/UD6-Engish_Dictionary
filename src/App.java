df79be3c0cb37a4dcca40cf0aeec20749f40f79a
the-albertox <126242013+The-Albertox@users.noreply.github.com>
2024-05-07 13:02:23 +0100

    fear(menu): add first method

--- src/App.java
+++ src/App.java
@@ -1,24 +1,39 @@
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 import app.salesianos.resource.Menu;
+import app.salesianos.wordQualifier.WordQualifier;
 
 public class App {
     static final Scanner KEYBOARD = new Scanner(System.in);
-    public static void main(String[] args) throws Exception {
-    boolean exist = true;
-    String option= "";
+    static final WordQualifier qualifier = new WordQualifier();
 
-    Menu menu = new Menu();
-    while (exist) {
-        option= menu.getMenu();
-        switch (option) {
-            case "1":
-                
-                break;
-        
-            default:
-                break;
+    public static void main(String[] args) throws Exception {
+        boolean exist = true;
+        String option = "";
+
+        Menu menu = new Menu();
+        while (exist) {
+            option = menu.getMenu();
+            switch (option) {
+                case "1":
+                    AddWord();
+                    break;
+
+                default:
+                    break;
+            }
         }
+
     }
+
+    public static void AddWord() {
+        JOptionPane.showInputDialog(null, "introduzca la palabra");
+        String wordToAdd = KEYBOARD.nextLine();
+        qualifier.addWord(wordToAdd);
+        JOptionPane.showMessageDialog(null, "la palabra se ha agregado correctamente");
     }
+
+   
 }
