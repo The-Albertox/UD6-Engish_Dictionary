7f411c2a494244166267863bdcdcb07ad3f3e0c8
the-albertox <126242013+The-Albertox@users.noreply.github.com>
2024-05-07 12:42:12 +0100

    feat(menu): initialize menu innapp

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
+import app.salesianos.resource.Menu;
+
 public class App {
+    static final Scanner KEYBOARD = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    boolean exist = true;
+    String option= "";
+
+    Menu menu = new Menu();
+    while (exist) {
+        option= menu.getMenu();
+        switch (option) {
+            case "1":
+                
+                break;
+        
+            default:
+                break;
+        }
+    }
     }
 }
